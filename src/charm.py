604771111939d7889668eb2b360ebffbeece6677
ggouzi <gaetan.gouzi@canonical.com>
2024-10-22 08:40:35 +0200

    feat: Install usg package during the installation phase

--- src/charm.py
+++ src/charm.py
@@ -13,16 +13,17 @@ https://juju.is/docs/sdk/create-a-minimal-kubernetes-charm
 """
 
 import logging
-from typing import cast
 import tempfile
 import base64
 import subprocess
 import ops
+import charmhelpers.fetch as fetch
 
 # Log messages can be retrieved using juju debug-log
 logger = logging.getLogger(__name__)
 
 VALID_LOG_LEVELS = ["info", "debug", "warning", "error", "critical"]
+USG_PACKAGE = "usg"
 
 
 class CharmCisHardeningCharm(ops.CharmBase):
@@ -36,21 +37,32 @@ class CharmCisHardeningCharm(ops.CharmBase):
         framework.observe(self.on.execute_audit_action, self._on_audit_action)
 
     def _on_install(self, event):
+        self.install_usg()
         self.unit.status = ops.ActiveStatus("Ready to execute CIS hardening.")
         if self.model.config["auto-harden"]:
             self.cis_harden()
 
     def _on_audit_action(self, event):
         self.audit("/tmp/audit.results")
-            
+
     def audit(self, results_file):
-        return subprocess.check_output(f"usg audit --tailoring-file {results_file}".split(" ")).decode('utf-8')
+        return subprocess.check_output(
+            f"usg audit --tailoring-file {results_file}".split(" ")
+        ).decode("utf-8")
+
+    def install_usg(self):
+        pkg = USG_PACKAGE
+        self.unit.status = ops.MaintenanceStatus(f"Installing {pkg}")
+        fetch.apt_install([pkg], fatal=True)
+        self.unit.status = ops.ActiveStatus("Ready to execute CIS hardening")
 
     def cis_harden(self):
         with tempfile.NamedTemporaryFile("w", delete=False) as fh:
-            fh.write(base64.b64decode(self.model.config["tailoring-file"]).decode('utf-8'))
+            fh.write(base64.b64decode(self.model.config["tailoring-file"]).decode("utf-8"))
             fh.flush()
-            return subprocess.check_output(f"usg fix --tailoring-file {fh.name}".split(" ")).decode('utf-8')
+            return subprocess.check_output(
+                f"usg fix --tailoring-file {fh.name}".split(" ")
+            ).decode("utf-8")
 
     def _cis_harden_action(self, event):
         self.unit.status = ops.ActiveStatus("Executing hardening...")
